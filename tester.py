1abd3a93aa9d48e66ef74e58b177315b33ae0716
Arpit Sharma <32858287+Arpit-Sharma-USC@users.noreply.github.com>
2018-02-17 06:09:15 -0800

    Delete tester.py

--- tester.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas
-import copy
-from sklearn.feature_selection import RFE
-from sklearn.ensemble import ExtraTreesRegressor
-
-dataframe = pandas.read_csv("C:/Users/Shanu/PycharmProjects/Forest_Fire/forestfires.csv")
-print(list(dataframe))
